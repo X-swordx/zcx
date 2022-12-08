33e3ee1e237f0e57424398366a05b447518fc49c
zhengchengxuan <534370078@qq.com>
2022-12-08 14:59:20 +0800

    feat: 重构docs

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -1,22 +1,25 @@
 export default {
   "/fe/": [
     {
-      text: "📓 前端算法",
+      text: "📓 前端面试",
       collapsible: true,
       collapsed: false,
-      items: [{ text: "导读", link: "/fe/algorithm/ch0" }],
+      items: [{ text: "20个常见的算法题", link: "/fe/interview/20-algorithm" }],
     },
     {
       text: "🌱 编程学习",
       collapsible: true,
       collapsed: false,
-      items: [{ text: "发布 npm 包", link: "/fe/program/ch0" }],
+      items: [{ text: "发布 npm 包", link: "/fe/program/dumi-npm" }],
     },
     {
       text: "📝 备忘录",
       collapsible: true,
       collapsed: false,
-      items: [{ text: "Git 命令", link: "/fe/memo/ch0" }],
+      items: [
+        { text: "Git 命令", link: "/fe/memo/git" },
+        { text: "vitepress + github page", link: "/fe/memo/vitepress" }
+      ],
     },
   ],
 };
